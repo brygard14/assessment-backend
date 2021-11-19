5255397599d29f71b7e5d75061f6321a69a321d0
brygard14 <bryana.gardner@gmail.com>
2021-11-19 11:37:31 -0700

    completed assessment sort of

--- server/index.js
+++ server/index.js
@@ -3,23 +3,38 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
+  const compliments = [
+    "Gee, you're a smart cookie!",
+    "Cool shirt!",
+    "Your Javascript skills are stellar.",
   ];
-
   // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
 });
+
+app.get("/api/fortune", (req, res) => {
+  console.log(req.query)
+  console.log(res)
+  // const fortunes = [
+  //   "A beautiful, smart, and loving person will be coming into your life.",
+  //   "A fresh start will put you on your way.",
+  //   "A good time to finish up old tasks.",
+  //   "A lifetime of happiness lies ahead of you.",
+  // ];
+  // //choose random fortune
+  // let randomIndex = Math.floor(Math.random() * fortunes.length);
+  // let randomFortune = fortunes[randomIndex];
+
+  // res.status(200).send(randomFortune);
+});
+
+app.put()
 
 app.listen(4000, () => console.log("Server running on 4000"));
